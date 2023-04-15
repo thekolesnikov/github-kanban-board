9f481213d5f25c04b4ec977b3e4edd769d5eb1be
thekolesnikov <thesharkov@gmail.com>
2023-04-15 16:26:35 +0300

    add drag-n-drop

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,18 +1,37 @@
 import { timeAgo } from '../../utils/timeAgo.js';
 
-function TodoItem({ id, title, number, login, comments, created }) {
+function TodoItem({
+    title,
+    number,
+    login,
+    comments,
+    created,
+    onDragOver,
+    onDragLeave,
+    onDragStart,
+    onDragEnd,
+    onDrop,
+}) {
     const date = timeAgo(created);
     return (
-        <div key={id} className="todoitem" draggable="true">
-            <div>{title}</div>
-            <div className="todoiten__flex">
-                <p>#{number}</p>
-                <p>{date}</p>
+        <div
+            className="todoitem"
+            draggable="true"
+            onDragOver={onDragOver}
+            onDragLeave={onDragLeave}
+            onDragStart={onDragStart}
+            onDragEnd={onDragEnd}
+            onDrop={onDrop}
+        >
+            <div className="todoitem__el">{title}</div>
+            <div className="todoitem__flex">
+                <p className="todoitem__el">#{number}</p>
+                <p className="todoitem__el">{date}</p>
             </div>
-            <div className="todoiten__flex">
-                <p>{login}</p>
-                <span>|</span>
-                <p>Comments: {comments}</p>
+            <div className="todoitem__flex">
+                <p className="todoitem__el">{login}</p>
+                <span className="todoitem__el">|</span>
+                <p className="todoitem__el">Comments: {comments}</p>
             </div>
         </div>
     );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,92 +1,138 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import TodoItem from '../TodoItem/TodoItem';
 
 function TodoList() {
-    const issues = useSelector((state) => state.issues.issues);
+    const [boards, setBoards] = useState([
+        { name: 'All Todo', items: [] },
+        { name: 'In Progress', items: [] },
+        { name: 'Done', items: [] },
+    ]);
+
     const repoInfo = useSelector((state) => state.issues.repo);
+    const allIssues = useSelector((state) => state.issues.issues);
+
+    const inProgressIssues = allIssues.filter(
+        (issue) => issue.state === 'open'
+    );
+    const doneIssues = allIssues.filter((issue) => issue.state === 'closed');
+
+    useEffect(() => {
+        setBoards([
+            { name: 'All Todo', items: [...allIssues] },
+            { name: 'In Progress', items: [...inProgressIssues] },
+            { name: 'Done', items: [...doneIssues] },
+        ]);
+    }, [repoInfo]);
+
+    const [currentBoard, setCurrentBoard] = useState(null);
+    const [currentItem, setCurrentItem] = useState(null);
+
+    // Drag functions
+    function dragOverHandlerEvent(e) {
+        e.preventDefault();
+    }
+
+    function dragLeaveHandler(e) {}
+
+    function dragStartHandler(e, board, item) {
+        setCurrentBoard(board);
+        setCurrentItem(item);
+    }
+
+    function dragEndHandler(e) {}
+
+    function dropHandler(e, board, item) {
+        e.preventDefault();
+        e.stopPropagation();
+        const currentIndex = currentBoard.items.indexOf(currentItem);
+        currentBoard.items.splice(currentIndex, 1);
+        const dropIndex = board.items.indexOf(item);
+        board.items.splice(dropIndex + 1, 0, currentItem);
+        setBoards(
+            boards.map((b) => {
+                if (b.name === board.name) {
+                    return board;
+                }
+                if (b.name === currentBoard.name) {
+                    return currentBoard;
+                }
+                return b;
+            })
+        );
+    }
+
+    function dropCardHandler(e, board) {
+        board.items.push(currentItem);
+        const currentIndex = currentBoard.items.indexOf(currentItem);
+        currentBoard.items.splice(currentIndex, 1);
+        setBoards(
+            boards.map((b) => {
+                if (b.name === board.name) {
+                    return board;
+                }
+                if (b.name === currentBoard.name) {
+                    return currentBoard;
+                }
+                return b;
+            })
+        );
+    }
 
     return (
         <div className="todolist">
             {repoInfo.error && <span className="error">Invalid URL</span>}
-            <div className="todolist__column">
-                {repoInfo.id && (
-                    <>
-                        <div className="todolist__title">ToDo</div>
-                        <div className="todolist__todos">
-                            {issues.length ? (
-                                issues.map((issue) => {
-                                    return (
-                                        <TodoItem
-                                            id={issue.id}
-                                            title={issue.title}
-                                            number={issue.number}
-                                            login={issue.user.login}
-                                            comments={issue.comments}
-                                            created={issue.created_at}
-                                        />
-                                    );
-                                })
-                            ) : (
-                                <span>No todo</span>
-                            )}
-                        </div>
-                    </>
-                )}
-            </div>
-            <div className="todolist__column">
-                {repoInfo.id && (
-                    <>
-                        <div className="todolist__title">In Progress</div>
-                        <div className="todolist__todos">
-                            {issues.length ? (
-                                issues.map((issue) => {
-                                    if (issue.state === 'open') {
+            {repoInfo.id &&
+                boards.map((board) => {
+                    return (
+                        <div
+                            key={board.name}
+                            className="todolist__column"
+                            onDragOver={(e) => dragOverHandlerEvent(e)}
+                            onDrop={(e) => dropCardHandler(e, board)}
+                        >
+                            <div className="todolist__title">{board.name}</div>
+                            <div className="todolist__todos">
+                                {board.items.length ? (
+                                    board.items.map((item) => {
                                         return (
                                             <TodoItem
-                                                id={issue.id}
-                                                title={issue.title}
-                                                number={issue.number}
-                                                login={issue.user.login}
-                                                comments={issue.comments}
-                                                created={issue.created_at}
+                                                onDragOver={(e) =>
+                                                    dragOverHandlerEvent(e)
+                                                }
+                                                onDragLeave={(e) =>
+                                                    dragLeaveHandler(e)
+                                                }
+                                                onDragStart={(e) =>
+                                                    dragStartHandler(
+                                                        e,
+                                                        board,
+                                                        item
+                                                    )
+                                                }
+                                                onDragEnd={(e) =>
+                                                    dragEndHandler(e)
+                                                }
+                                                onDrop={(e) =>
+                                                    dropHandler(e, board, item)
+                                                }
+                                                key={item.id}
+                                                id={item.id}
+                                                title={item.title}
+                                                number={item.number}
+                                                login={item.user.login}
+                                                comments={item.comments}
+                                                created={item.created_at}
                                             />
                                         );
-                                    }
-                                })
-                            ) : (
-                                <span>No todo</span>
-                            )}
+                                    })
+                                ) : (
+                                    <span>No todo</span>
+                                )}
+                            </div>
                         </div>
-                    </>
-                )}
-            </div>
-            <div className="todolist__column">
-                {repoInfo.id && (
-                    <>
-                        <div className="todolist__title">Done</div>
-                        <div className="todolist__todos">
-                            {issues.length ? (
-                                issues.map((issue) => {
-                                    if (issue.state === 'closed') {
-                                        return (
-                                            <TodoItem
-                                                id={issue.id}
-                                                title={issue.title}
-                                                number={issue.number}
-                                                login={issue.user.login}
-                                                comments={issue.comments}
-                                                created={issue.created_at}
-                                            />
-                                        );
-                                    }
-                                })
-                            ) : (
-                                <span>No todo</span>
-                            )}
-                        </div>
-                    </>
-                )}
-            </div>
+                    );
+                })}
         </div>
     );
 }
